38ceb4e317356d7177d4cfa23d96c13c8ac51baf
ownersmodmenu <135044585+ownersmodmenu@users.noreply.github.com>
2023-06-09 12:43:08 -0500

    Create PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+namespace Bark
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.kylethescientist.gorillatag.bark";
+        public const string Name = "Krab";
+        public const string Version = "1.2.1";
+    }
+}
